6210a1b9155d528642996db2a5e80c2c43d5a4c8
Midnitte <midnitte@gmail.com>
2023-07-22 14:16:33 -0400

    Completed ini6

--- Stronghold Problems/ini6.py
+++ Stronghold Problems/ini6.py
@@ -4,10 +4,11 @@ def wordNums(s: str) -> dict:
     """Given: A string s of length at most 10000 letters.
     Return: The number of occurrences of each word in s, where words are separated by spaces. Words are case-sensitive, and the lines in the output can be in any order."""
     wordsCount = {}
-    for word in s.split(' '):
+    for word in s.strip("\n").split(' '):
         wordsCount[word] = wordsCount.setdefault(word, 0) + 1
     for word in wordsCount:
         print(f"{word} {wordsCount[word]}")
+
 
 example = "We tried list and we tried dicts also we tried Zen"
 
